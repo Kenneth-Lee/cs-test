c68fefb603138687450138eed28112c5089a98f9
Kenneth Lee <liguozhu@hisilicon.com>
2024-09-12 10:31:51 +0800

    ctx_switch: optimize render script
    Signed-off-by: Kenneth Lee <liguozhu@hisilicon.com>

--- ctx_switch_test.sh
+++ ctx_switch_test.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
 
 for i in `seq 2 20`; do
-	sudo perf stat -e itlb_misses.walk_completed ./ctx_switch -M 10000 -n $i
+	sudo perf stat --cpu 2 -e itlb_misses.walk_completed ./ctx_switch -l 1 -M 10000 -n $i
 done
--- render_ctx_switch.py
+++ render_ctx_switch.py
@@ -4,9 +4,6 @@ import sys
 import re
 import matplotlib.pyplot as plt
 
-with open(sys.argv[1]) as f:
-    lines=f.readlines()
-
 num="([1234567890.,]+)"
 re_cpu_core=re.compile(num+".*cpu_core")
 re_time_elapsed=re.compile(num+".*time elapsed")
@@ -19,28 +16,40 @@ def match_line(a, rec, line):
         return True
     return False
 
-core_counter=[]
-time_counter=[]
-ave_core_counter=[]
-x=[]
+def render_one_result(ax, fn):
+    core_counter=[]
+    time_counter=[]
+    ave_core_counter=[]
+    x=[]
 
-for line in lines:
-    if match_line(core_counter, re_cpu_core, line):
-        continue
-    else:
-        match_line(time_counter, re_time_elapsed, line)
+    with open(fn) as f:
+        lines=f.readlines()
 
-if len(core_counter) != len(time_counter):
-    print(f"format error, core/time counter is {core_counter}/{time_counter}")
-    sys.exit(-1)
+    for line in lines:
+        if match_line(core_counter, re_cpu_core, line):
+            continue
+        else:
+            match_line(time_counter, re_time_elapsed, line)
 
-for i in range(len(core_counter)):
-    ave_core_counter.append(core_counter[i]/time_counter[i]) # assume no zero in time_counter
-    x.append(i+2)
+    if len(core_counter) != len(time_counter):
+        print(f"format error, core/time counter is {core_counter}/{time_counter}")
+        sys.exit(-1)
+
+    for i in range(len(core_counter)):
+        ave_core_counter.append(core_counter[i]/time_counter[i]) # assume no zero in time_counter
+        x.append(i+2)
+
+    l = ax.plot(x, ave_core_counter, label=fn)
+    return l
 
 fig, ax=plt.subplots()
-ax.plot(x, ave_core_counter)
 ax.set(xlabel="num_process", ylabel="ave itlb misses", title="tlbmiss with switch process number")
 ax.grid()
-#fig.savefig("test.png")
+
+ls = []
+for fn in sys.argv[1:]:
+    l = render_one_result(ax, fn)
+    ls.append(l)
+
+fig.legend()
 plt.show()
